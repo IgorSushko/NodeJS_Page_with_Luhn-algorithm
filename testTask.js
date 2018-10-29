f4d456bf1a3063c9c127a1190bea39ad85f1b904
ksu <ksu.zhytomirsky@gmail.com>
2018-10-29 13:28:33 +0200

    formatted in eslint

--- testTask.js
+++ testTask.js
@@ -1,73 +1,65 @@
-const http = require('http')
-const fs = require('fs')
-const fastluhn=require('fast-luhn')
+const http = require('http');
+const fs = require('fs');
+const fastluhn = require('fast-luhn');
 
-const PORT=8080;
+const PORT = 8080;
 
 
-const server =http.createServer(function(request,response){
+const server = http.createServer((request, response) => {
+  console.log(`Request was made :${request.url}`);
+  console.log(`Request method :${request.method}`);
 
-        console.log ('Request was made :' + request.url);
-        console.log ('Request method :' + request.method);
+  if (request.method == 'GET') {
+    if (request.url === '/') {
+      response.writeHeader(200, { 'Content-Type': 'text/html' });
 
-     if (request.method == "GET") {
-            if (request.url==='/') {
-                    response.writeHeader(200, {"Content-Type": "text/html"}); 
-                    
-                    var myHtml=fs.readFile('./TestTask/index.html','utf8',function(error, data){
-                 
-                        const answerValue = "Your result will be shown here"; 
-                        data = data.toString().replace('{answerValue}', answerValue);
-                        response.end(data);}); 
-                } 
+      const myHtml = fs.readFile('./TestTask/index.html', 'utf8', (error, data) => {
+        const answerValue = 'Your result will be shown here';
+        data = data.toString().replace('{answerValue}', answerValue);
+        response.end(data);
+      });
+    } else if (request.url === '/Style.css') {
+      response.writeHeader(200, { 'Content-Type': 'text/css' });
+      const myHtml = fs.createReadStream('./TestTask/Style.css', 'utf8');
+      myHtml.pipe(response);
+    } else if (request.url === '/favicon.png' || '/favicon.ico') {
+      console.log('Request method for favicon');
+      response.writeHeader(200, { 'Content-Type': 'image/png' });
+      const myHtml = fs.createReadStream('./TestTask/favicon.png');
+      myHtml.pipe(response);
+    }
+  } else if (request.method == 'POST') {
+    console.log(`Request method :${request.method}`);
+    request.on('data', (chunk) => {
+      const formdata = chunk.toString();
+      const number = eval(formdata.split('&')[0]);
+      console.log(number);
 
-            else if (request.url==='/Style.css') {
-                    response.writeHeader(200, {"Content-Type": "text/css"});  
-                    const myHtml=fs.createReadStream('./TestTask/Style.css','utf8');
-                    myHtml.pipe(response);
-                }
-            else if (request.url==='/favicon.png' ||'/favicon.ico' ) {
-                        console.log('Request method for favicon');
-                        response.writeHeader(200, {"Content-Type": "image/png"});  
-                        const myHtml=fs.createReadStream('./TestTask/favicon.png');
-                        myHtml.pipe(response);
-                } } 
-      else if (request.method == 'POST') {
+      const result = fastluhn(number.toString());
+      let showingResult = '';
+      if (result == true) {
+        showingResult = `Card number: ${number.toString()} is valid `;
+      } else {
+        showingResult = `Card number: ${number.toString()} is NOT valid `;
+      }
 
-            console.log('Request method :' + request.method); 
-            request.on('data', function(chunk) {
-
-                let formdata = chunk.toString();      
-                let number = eval(formdata.split("&")[0]);
-                console.log(number);
-
-                let result = fastluhn(number.toString());
-                var showingResult=''; 
-                    if (result==true) {
-                            showingResult='Card number: '+number.toString()+' is valid ';
-                                       }  
-                         else {
-                            showingResult='Card number: '+number.toString()+' is NOT valid ';
-                                       }   
-                           
-                       response.setHeader('Content-Type', 'text/html');
-                       var myHtmlresp=fs.readFile('./TestTask/index.html','utf8',function(error, data) {
-                            const answerValue = showingResult; 
-                            data = data.toString().replace('{answerValue}', answerValue);
-                            response.end(data);});
-                       //response.writeHead(200);
-                        }) //response.end(form);
-
-        } else {
-             response.writeHead(200);
-             response.end();
-              };
-
+      response.setHeader('Content-Type', 'text/html');
+      const myHtmlresp = fs.readFile('./TestTask/index.html', 'utf8', (error, data) => {
+        const answerValue = showingResult;
+        data = data.toString().replace('{answerValue}', answerValue);
+        response.end(data);
+      });
+      // response.writeHead(200);
+    }); // response.end(form);
+  } else {
+    response.writeHead(200);
+    response.end();
+  }
 }).listen(PORT);
 
-  
-//http://localhost:8080/
+
+// http://localhost:8080/
 
 
 // example card number  == 4532015112830366
-//https://www.npmjs.com/package/fast-luhn
+// https://www.npmjs.com/package/fast-luhn
